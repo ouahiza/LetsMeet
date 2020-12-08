ec19f75ef64a06317b0f1a094960db57838bb2e5
Dalmeidaagrippine <62750119+Dalmeidaagrippine@users.noreply.github.com>
2020-12-08 09:14:58 +0100

    Api sondage

---
+++ src/test/java/ApiSurvey.java
@@ -0,0 +1,16 @@
+import com.LetsMeet.Application;
+import com.LetsMeet.models.User;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
+
+
+public class ApiSurvey {
+}
